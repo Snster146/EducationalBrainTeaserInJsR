b5f5d4442bfe593dfdf7114bbcef9ae81473b858
Snster146 <sonnynormansell@gmail.com>
2025-03-27 09:51:31 +0000

    Update GeoBase.js

--- javascript/GeoBase.js
+++ javascript/GeoBase.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $("#b1").click(function(){
-        window.location.href="html/flagguesshome.html";
+        window.location.href="../html/flagguesshome.html";
     });
     $("#b2").click(function(){
-        window.location.href="CountryGuessinghome.html";
+        window.location.href="../CountryGuessinghome.html";
     });
     $("#b3").click(function(){
-        window.location.href="html/CapitalGuessingHome.html";
+        window.location.href="../html/CapitalGuessingHome.html";
     });
     $("#b4").click(function(){
-        window.location.href="index.html";
+        window.location.href="../index.html";
     });
 
 });
